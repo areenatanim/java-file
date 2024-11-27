5613adcec1a46ad9678f872606a15e64338a5525
areenatanim <areenatanim@gmail.com>
2024-11-27 10:48:31 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,16 @@
+ console.log(222);
+var price=333;
+console.log(price);
+var apple=20;
+apple=30
+var orange= parseInt('45');
+var pan= parseFloat('22.4')
+console.log(pan);
+
+console.log(typeof orange);
+console.log(typeof apple);
+
+
+console.log(apple+orange);
+console.log(10%3);
+
