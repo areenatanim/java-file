6a548d53e9f6ccb33a39aeec2764f8a917c35887
areenatanim <areenatanim@gmail.com>
2024-12-17 22:08:25 +0600

    nested.js added

--- intro.js
+++ intro.js
@@ -1,6 +1,26 @@
-var orange = 650;
+//  practice problem-01
+ var orange = 650;
 var apple = 650;
 var totale = orange + apple;
 // totale - = 1000;
 console.log(totale - 1000);
+
+// practice problem-02
+var Mathematice = 75.25;
+var Biology = 65;
+var Chemistry = 80;
+var Physics = 35.45;
+var Bangla = 99.50;
+var totale = Mathematice + Biology + Chemistry + Physics + Bangla;
+var average = totale / 5;
+console.log(average)
+console.log(average .toFixed(2));
+// practice problem-03
+var a = 119;
+var b = 5;
+console.log(a % b);
+// practice problem-04
+// const a = isNaN ('11')
+// console.log(a);
+
 
